26f23352f1f7f681d95502f5844d1e0ead2420e9
xuyimin <xuyimin@stu.csust.edu.cn>
2019-11-25 22:59:55 +0800

    bookController更改

--- src/main/java/cn/analysys/douban/controller/BookController.java
+++ src/main/java/cn/analysys/douban/controller/BookController.java
@@ -4,9 +4,13 @@ import cn.analysys.douban.pojo.Book;
 import cn.analysys.douban.pojo.BookEssay;
 import cn.analysys.douban.pojo.BookReview;
 import cn.analysys.douban.service.BookService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,26 +23,33 @@ import java.util.List;
  * @since JDK 1.8
  */
 
+@Api("书籍接口")
+@RequestMapping("/book")
 @RestController
 public class BookController {
 
     @Autowired
     BookService bookService;
 
-    @GetMapping("/book")
+    @ApiOperation(value = "最火的读书类目小说评论top10",notes = "无接受参数")
+    @GetMapping("")
     public List<Book> showBook(){
         List<Book> listbooks=bookService.selBook();
         return listbooks;
     }
 
-    @GetMapping ("/bookEssay/{id}")
-    public List<BookEssay> showBookEssay(@PathVariable("id") int id){
+    @ApiOperation(value = "获取书评详情",notes = "根据影评id获取影评详情信息")
+    @ApiImplicitParam(name = "essayId",value = "影评id",dataType = "String",required = true)
+    @GetMapping ("/essay/{essayId}")
+    public List<BookEssay> showBookEssay(@PathVariable("essayId") int id){
         List<BookEssay> listBookEssay=bookService.selBookEssay(id);
         return  listBookEssay;
     }
 
-    @GetMapping("/bookReview/{id}")
-    public List<BookReview> showBookReview(@PathVariable("id") int id){
+    @ApiOperation(value = "获取书籍基本信息和短评",notes = "根据书籍id获取基本信息和短评")
+    @ApiImplicitParam(name = "reviewId",value = "查询短评id",dataType = "int",required = true)
+    @GetMapping("/review/{reviewId}")
+    public List<BookReview> showBookReview(@PathVariable("reviewId") int id){
         List<BookReview> listBookReview=bookService.selBookReview(id);
         return listBookReview;
     }
